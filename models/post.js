542b45b726a867e0b3f883f26aaf6695ba12a2cc
Som Ramnani <som.ramnani@gmail.com>
2019-10-28 16:05:56 -0500

    adding image

---
+++ models/post.js
@@ -0,0 +1,12 @@
+module.exports = function(sequelize, DataTypes) {
+  var Post = sequelize.define("Post", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+  return Post;
+};
