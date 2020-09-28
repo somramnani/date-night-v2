ffdfe4ce693712e37f9337f2b5d7ea851e402a8a
Som Ramnani <som.ramnani@gmail.com>
2020-09-28 11:44:09 -0400

    deletes passport from the application: deleted middleware isAuthenticated

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,39 +1,13 @@
 var path = require("path");
 
-// Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
 //
-module.exports = function(app) {
-  app.get("/", function(req, res) {
-    // If the user already has an account send them to the members page
-    // if (req.user) {
-    //   res.redirect("/members");
-    // }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
-
-  app.get("/login", function(req, res) {
-    // If the user already has an account send them to the members page
-    // if (req.user) {
-    //   res.redirect("/members");
-    // }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
-
-  app.get("/signup", function(req, res) {
-    // If the user already has an account send them to the members page
-    // if (req.user) {
-    //   res.redirect("/members");
-    // }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
-  });
-
-  app.get("/members", isAuthenticated, function(req, res) {
+module.exports = function (app) {
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
 
   //IMAGE ROUTES
-  app.get("/backgroundimage", isAuthenticated, function(req, res) {
+  app.get("/backgroundimage", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/images/rps.jpg"));
   });
 };
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require('cors');
-const apiRoutes = require('./routes/api-routes');
-const pageRoutes = require('./routes/html-routes');
+const cors = require("cors");
+const apiRoutes = require("./routes/api-routes");
+const pageRoutes = require("./routes/html-routes");
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
 const app = express();
@@ -12,11 +12,15 @@ app.use(cors);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use('/api/', apiRoutes);
-app.use('/pages/', pageRoutes);
+app.use("/api/", apiRoutes);
+app.use("/pages/", pageRoutes);
 
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
   });
 });
