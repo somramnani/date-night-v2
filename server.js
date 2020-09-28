3808e6e5fb7bd12b1ed3e49bd400f3cf05ec60ef
Brent Abruzese <brent.abruzese@gmail.com>
2020-09-28 09:51:28 -0400

    removed passport middleware from server

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const session = require("express-session");
 const cors = require('cors');
-const passport = require("./config/passport");
 
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
@@ -13,10 +11,6 @@ app.use(cors)
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
-
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
 
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
