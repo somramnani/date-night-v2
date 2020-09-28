835c027deaba5193ac513651111962d9b466a0a6
Brent Abruzese <brent.abruzese@gmail.com>
2020-09-28 09:37:48 -0400

    added cors middleware

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const session = require("express-session");
-
+const cors = require('cors');
 const passport = require("./config/passport");
 
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
-
 const app = express();
+
+app.use(cors)
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(express.static("public"));
 
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
@@ -19,7 +20,6 @@ app.use(passport.session());
 
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
-
 
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
