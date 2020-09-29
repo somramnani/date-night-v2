cf32d4f61375f47a565652446ec8e928e397628f
Som Ramnani <som.ramnani@gmail.com>
2020-09-29 13:50:59 -0400

    edited the search bar in the homepage

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,16 +1,16 @@
 const path = require("path");
 const router = require("express").Router();
 
-router.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/login.html"))
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/login.html"));
 });
 
-router.get('/results', (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/results.html"))
+router.get("/results", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/results.html"));
 });
 
-router.get('/signup', (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/signup.html"))
+router.get("/signup", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/signup.html"));
 });
 
 router.get("/styles", function (req, res) {
