7d298831a3b4fbf0f8e09641087fd520c17e252b
Som Ramnani <som.ramnani@gmail.com>
2020-09-29 11:00:40 -0400

    finished the homepage, added a picture, searchbar, and bootstrap styling

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,7 +3,10 @@ var path = require("path");
 //
 module.exports = function (app) {
   app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+  app.get("/results", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/results.html"));
   });
 
   app.get("/styles", function (req, res) {
@@ -15,6 +18,13 @@ module.exports = function (app) {
 
   //IMAGE ROUTES
   app.get("/backgroundimage", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/images/rps.jpg"));
+  });
+  app.get("/fireworks", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/images/fireworks.jpg"));
+  });
+
+  app.get("/datenight", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/images/rps.jpg"));
   });
 };
