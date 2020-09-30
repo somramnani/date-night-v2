5b71e7a400ce28467834abd52fc2e30aeb477e33
Brent Abruzese <brent.abruzese@gmail.com>
2020-09-30 09:56:38 -0400

    more work to the api

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,12 +3,11 @@ const router = require('express').Router();
 const axios = require('axios');
 const yelpKey = process.env.YELP_TOKEN;
 
-router.post(`/get-date-data`, (req, res) => {
-  const { location, budget, startDate, endDate, dateType } = req.body;
+router.post(`/get-date-data`, async (req, res) => {
+  const { location, budget, startDate, dateType } = req.body;
   const baseURL = `https://api.yelp.com/v3`;
   let restaurantURL = `/businesses/search?location=${location}&price=${budget}`;
   let eventURL = `/events?location=${location}`;
-  //TODO: Add google places for theater data
 
   //axios instance for yelp requests
   const instance = axios.create({
@@ -16,19 +15,44 @@ router.post(`/get-date-data`, (req, res) => {
     headers: { Authorization: `Bearer ${yelpKey}` }
   });
 
-  const getRestaurants = () => instance.get(restaurantURL);
-  const getEvents = () => instance.get(eventURL);
+  // const getRestaurants = () => instance.get(restaurantURL);
+
+
+    switch(dateType) {
+      case 'exciting': { 
+        const getRestaurants = () => instance.get(eventURL + '&categories=nightlife,active')
+        const getEvents = () => instance.get(restaurantURL + '&categories=bars')
+
+        Promise.all([getRestaurants(), getEvents()])
+          .then(results => {
+            let restaurants = results[0].data.restaurants;
+            let events = results[1].data.events;
+
+            console.log(results);
+          })
+      }
+      break;
+      case 'romantic': {
+        instance.get(eventURL + '&categories=wine-tasting,food-and-drink');
+        instance.get(restaurantURL + '&categories=wine_bars,french,winetastingroom')
+      }
+      break;
+      case 'relaxing': {
+        instance.get(eventURL + '&categories=wine-tasting,food-and-drink');
+        instance.get(restaurantURL + '&categories=wine_bars,french,winetastingroom')
+      }
+    }
 
   //fetch the data concurrently and send to client
-  Promise.all([getRestaurants(), getEvents()])
-    .then(results => { 
-      const restaurants = results[0].data;
-      const events = results[1].data;
-
-      //TODO: this is where the conditional date type logic goes...
-      res.json({ restaurants, events });
-    })
-    .catch(error => console.log(error));
+  // Promise.resolve()
+  //   .then(results => { 
+  //     let restaurants = results[0].data.restaurants;
+  //     let events = results[1].data.events;
+            
+  //     console.log(results)
+      
+  //   })
+  //   .catch(error => console.log(error));
 });
 
 module.exports = router;
