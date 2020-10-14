8647b1a7c8f9b75d5c5935e129842c551a6312aa
Som Ramnani <som.ramnani@gmail.com>
2020-10-14 12:24:06 -0400

    adds dropdown functionality in the results page -- displayDropDown() function

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -93,3 +93,25 @@ searchBarCalender.value = moment().format("YYYY-MM-DD");
 window.onload = function () {
   document.getElementById("loading").style.display = "none";
 };
+
+//Shows dropdown content
+function displayDropDown() {
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  for (let i = 0; i < dropdowns.length; i++) {
+    var dropdown = dropdowns[i];
+    dropdown.classList.toggle("show");
+  }
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
