71c9ecac92809745fd3c6df8af4fa3dc78ea439e
Brent Abruzese <brent.abruzese@gmail.com>
2020-10-22 11:45:58 -0400

    itinerary creation and find functionality started on server

--- models/models.js
+++ models/models.js
@@ -1,5 +1,6 @@
 module.exports = function(sequelize, DataTypes) {
   const { STRING } = DataTypes;
+
   const User = sequelize.define("user", {
     oauthId: STRING,
     displayName: STRING,
@@ -7,13 +8,35 @@ module.exports = function(sequelize, DataTypes) {
     lastName: STRING,
     email: STRING,
     image: STRING,
-    itinerary: DataTypes.JSON
   });
 
   const Itinerary = sequelize.define("itinerary", {
-    restaurants: STRING,
-    events: STRING
+    oauthId: STRING,
+    activities: DataTypes.JSON
   });
+
+  const ActivityEvent = sequelize.define("event", {
+    yelpId: STRING,
+    thumbnail: STRING.BINARY,
+    title: STRING,
+    address: STRING,
+  });
+
+  const ActivityRestaurant = sequelize.define("restaurant", {
+    yelpId: STRING,
+    thumbnail: STRING.BINARY,
+    title: STRING,
+    address: STRING,
+    rating: STRING,
+    price: STRING,
+  });
+
+  Itinerary.hasMany(ActivityEvent, { as: 'events', foreignKey: 'id' });
+
+  ActivityEvent.belongsTo(Itinerary, { as: 'itineraries', foreignKey: 'id'});
+
+  User.hasMany(Itinerary, { as: 'itineraries', foreignKey: 'oauthId', targetKey: 'oauthId'});
+  Itinerary.belongsTo(User, { as: 'user', foreignKey: 'oauthId', targetKey: 'oauthId'});
 
   return User, Itinerary;
 };
