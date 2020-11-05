89b5b0142a2fd3cea2f72403ec10d94a0b0e9fe1
Som Ramnani <som.ramnani@gmail.com>
2020-11-05 12:18:24 -0500

    fixes login authentication signout

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -46,7 +46,7 @@ router.get("/logout", (req, res) => {
   var returnTo = req.protocol + "://" + req.hostname;
   var port = req.connection.localPort;
   if (port !== undefined && port !== 80 && port !== 443) {
-    returnTo += ":" + port;
+    returnTo = process.env.NODE_ENV === 'production' ? `${returnTo}/` : `${returnTo}`;
   }
   var logoutURL = new url.URL(
     util.format("https://%s/v2/logout", process.env.AUTH0_DOMAIN)
